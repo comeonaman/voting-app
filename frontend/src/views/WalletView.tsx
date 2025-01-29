7eebc5a1c505c2768adb67bc01ea41de67bf484d
aman <102567339+amankaran02@users.noreply.github.com>
2025-01-29 00:38:26 -0500

    main frontend build complete

---
+++ frontend/src/views/WalletView.tsx
@@ -0,0 +1,12 @@
+const WalletView = () => {
+	return (
+		<>
+			<div className="mb-8">
+				<h1 className="text-4xl font-bold">Your Wallet Information</h1>
+			</div>
+			<div>Wallet View</div>
+		</>
+	)
+}
+
+export default WalletView
